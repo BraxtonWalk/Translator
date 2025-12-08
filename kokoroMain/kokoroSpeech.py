342bf30b9989f96e511a93df7d5bd1e78b433bd7
Ben Ford <ben4ord@hotmail.com>
2025-12-07 17:53:19 -0700

    Final Changes

--- kokoroMain/kokoroSpeech.py
+++ kokoroMain/kokoroSpeech.py
@@ -1,11 +1,4 @@
 from kokoro import KPipeline
-import numpy as np
-import torch
-import simpleaudio as sa
-from IPython.display import display, Audio
-import soundfile as sf
-
-# Replace 'your_audio_file.mp3' with the actual path to your audio file
  
 LANGUAGES = {
     "EN": "a",
@@ -15,29 +8,28 @@ LANGUAGES = {
     "JP": "j"
 }
 
-def play_audio_raw(audio, sample_rate=24000):
-    if isinstance(audio, torch.Tensor):
-        audio = audio.detach().cpu().numpy()
-
-    audio_int16 = (audio * 32767).astype(np.int16)
-
-    play_obj = sa.play_buffer(audio_int16, 1, 2, sample_rate)
-    play_obj.wait_done()
-
 
 def main(text: str, inputLang: str = "EN"):
+   
+    #Access language to turn into audio
     lang_code = LANGUAGES[inputLang]
     print("Language:", lang_code)
     print("Text:", text)
+
+    #Initializes Kokor Model
     pipeline = KPipeline(lang_code=lang_code)
-    generator = pipeline(text, voice='af_heart')
+
+    #Calls Kokor model and sends through text to then be spoken
+    generator = pipeline(text, voice='am_eric')
     filename = ""
+
+    # Saving audio 
     for i, (gs, ps, audio) in enumerate(generator):
         print(i, gs, ps)
         filename = f'translation.wav'
        ## display(Audio(data=audio, rate=24000))
         sf.write(filename, audio, 24000)
-    print("out of For loop")
+    print("Audio Saved....")
     
 
 if __name__ == "__main__":
--- test.py
+++
@@ -1,7 +0,0 @@
-from huggingface_hub import snapshot_download
-
-# Choose a folder to store the model
-local_dir = r"C:\Users\Braxton\OneDrive\Desktop\College Classes\CS 385 - Natural Language Processing\FinalProject\WhisperSmall"
-
-# Download the entire repo snapshot
-snapshot_download(repo_id="Systran/faster-whisper-small", local_dir=local_dir)
